5c4b194e9e3885a37e2f9eac64cb35a673526fb5
kiva <git@kiva.lgbt>
2025-01-02 04:16:29 -0600

    importing

--- import-dns/main.tf
+++
@@ -1,7 +0,0 @@
-variable "getHashed" {
-  type = string
-}
-
-output "hashed" {
-  value = base64sha256(var.getHashed)
-}
--- import-dns/make-import-statement.ts
+++ import-dns/make-import-statement.ts
@@ -1,5 +1,5 @@
 import { $ } from "npm:zx@8.3.0";
-import type {AsyncResult }  from "./result.ts";
+import {Try, type AsyncResult }  from "./result.ts";
 import { DomainRecord, Flags } from "./types.ts";
 import { base64sha256 } from "./utils.ts";
 
@@ -21,12 +21,31 @@ async function getTofuOrTerraform(): AsyncResult<string> {
   return ErrTofuOrTerraformNotFound;
 }
 
+function parseData(record: DomainRecord){
+  const withDotIfURL = Try(()=> {
+    if (!record.data.endsWith(".")) {
+      let data = record.data
+      if (!data.includes("://")) {
+        data = "https://"+data
+      }
+      new URL(data)
+      return record.data + "."
+    }
+    return record.data
+  })
+  if (withDotIfURL instanceof Error) {
+    return record.data
+  }
+  return withDotIfURL
+}
+
 export async function makeImportStatement(args: Flags, record: DomainRecord): AsyncResult<string> {
   const command = await getTofuOrTerraform();
   if (command instanceof Error) {
     return command;
   }
-  const uncompressed = `${record.type} | ${record.name} | ${record.data}`;
+  const data = parseData(record)
+  const uncompressed = `${record.type} | ${record.name} | ${data}`;
   const keyUsed = uncompressed.length > 64 ? await base64sha256(uncompressed) : uncompressed
 
   return `${command} import -var-file=".tfvars" '${args.rootResource}.digitalocean_record.records["${keyUsed}"]' '${args.domain},${record.id}' || echo import failed for "${uncompressed}"`;
