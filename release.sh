a5b0768cea341a625cd5c8e6d1921ae09072d749
kiva <git@kiva.lgbt>
2025-01-03 15:59:02 -0600

    updated

--- release.sh
+++ release.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 CHANGE_TYPE=$(gum choose "major" "minor" "patch")
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-gh pr create -f -l=$CHANGE_TYPE --title=$BRANCH_NAME -e
-echo $CHANGE_TYPE $BRANCH_NAME
- 
+gh pr create -l=$CHANGE_TYPE --title=$BRANCH_NAME -e
