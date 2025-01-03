54cdff011362dbcb826ae4aaf43b778416e334c2
kiva <git@kiva.lgbt>
2025-01-03 16:02:18 -0600

    no-release

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CHANGE_TYPE=$(gum choose "major" "minor" "patch")
+CHANGE_TYPE=$(gum choose "major" "minor" "patch" "no-release")
 gh pr create -l=$CHANGE_TYPE --title=$BRANCH_NAME -e
